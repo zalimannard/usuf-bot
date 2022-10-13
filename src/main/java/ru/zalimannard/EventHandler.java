5f4dd8f0abc4bfe58049fb8ab50ef41bf57fd8da
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-13 03:02:50 +0300

    docs: Add Javadoc to the project
    Added documentation to the existing Main and EventHandler classes.

--- src/main/java/ru/zalimannard/EventHandler.java
+++ src/main/java/ru/zalimannard/EventHandler.java
@@ -2,13 +2,28 @@ package ru.zalimannard;
 
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * The type Event handler. It reacts to events that occurred in the Discord.
+ * @author Dmitry Kolesnikov
+ * @version 1.0
+ */
 public class EventHandler extends ListenerAdapter {
     private final String prefix;
 
+    /**
+     * Instantiates a new Event handler.
+     *
+     * @param prefix the prefix. The bot only responds to messages starting with this line.
+     */
     public EventHandler(String prefix) {
         this.prefix = prefix;
     }
 
+    /**
+     * Gets prefix of processed messages.
+     *
+     * @return the prefix of processed messages.
+     */
     public String getPrefix() {
         return prefix;
     }
--- src/main/java/ru/zalimannard/Main.java
+++ src/main/java/ru/zalimannard/Main.java
@@ -6,9 +6,19 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * The type Main. Needed to launch the application.
+ * @author Dmitry Kolesnikov
+ * @version 1.0
+ */
 public class Main {
     private static Logger logger = LogManager.getRootLogger();
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments. args[0] - prefix of the bot, args[1] - token of the bot.
+     */
     public static void main(String[] args) {
         if (args.length != 2) {
             logger.fatal("Invalid count of arguments: {}", args.length);

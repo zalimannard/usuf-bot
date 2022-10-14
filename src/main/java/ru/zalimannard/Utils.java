19180da22ff5c539df15340c96386c89bd65c5d5
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-14 21:56:14 +0300

    chore: Implement of the isNonNegativeIntegerNumber
    Implementation of a method to verify that a number is integer and
    non-negative: Utils.isNonNegativeIntegerNumber.

--- src/main/java/ru/zalimannard/Utils.java
+++ src/main/java/ru/zalimannard/Utils.java
@@ -11,6 +11,19 @@ public abstract class Utils {
      * @return true if the value is integer and non-negative number
      */
     public static Boolean isNonNegativeIntegerNumber(String value) {
-        return null;
+        if (value == null) {
+            return false;
+        }
+        if (value.length() == 0) {
+            return false;
+        }
+
+        String digits = "0123456789";
+        for (int i = 0; i < value.length(); ++i) {
+            if (!digits.contains(String.valueOf(value.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
     }
 }

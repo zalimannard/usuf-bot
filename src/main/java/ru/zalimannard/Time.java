ff8153126195a1b97f1efcf6c111e0a0f3b1d9ac
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-15 04:58:23 +0300

    chore: Implementation of methods of the Time class
    Implementation of all methods of the Time class. All tests pass
    successfully.

--- src/main/java/ru/zalimannard/Time.java
+++ src/main/java/ru/zalimannard/Time.java
@@ -1,5 +1,8 @@
 package ru.zalimannard;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Objects;
 
 /**
@@ -24,13 +27,13 @@ public class Time {
     public static final Time HOUR = new Time(3600000L);
 
     private Long milliseconds;
-
+    private static Logger logger = LogManager.getRootLogger();
 
     /**
      * Instantiates a new Time. The default value is 0 milliseconds.
      */
     public Time() {
-
+        setMilliseconds(0L);
     }
 
     /**
@@ -39,7 +42,13 @@ public class Time {
      * @param milliseconds the time value in milliseconds
      */
     public Time(Long milliseconds) {
-
+        if (milliseconds == null) {
+            setMilliseconds(0L);
+        } else if (milliseconds < 0L) {
+            setMilliseconds(0L);
+        } else {
+            setMilliseconds(milliseconds);
+        }
     }
 
     /**
@@ -48,7 +57,11 @@ public class Time {
      * @param other the time to be copied
      */
     public Time(Time other) {
-
+        if (other == null) {
+            setMilliseconds(0L);
+        } else {
+            setMilliseconds(other.getMilliseconds());
+        }
     }
 
     /**
@@ -57,7 +70,11 @@ public class Time {
      * @param timeAsString the value that the object may be initialized with
      */
     public Time(String timeAsString) {
-
+        if (isCorrectTime(timeAsString)) {
+            setMilliseconds(timeAsStringToLong(timeAsString));
+        } else {
+            setMilliseconds(0L);
+        }
     }
 
     /**
@@ -69,7 +86,57 @@ public class Time {
      * 1000, all not less than 0.
      */
     public static Boolean isCorrectTime(String value) {
-        return null;
+        if (value == null) {
+            logger.debug("Checking whether null is a date");
+            return false;
+        }
+
+        String[] hmsAndMsec = value.split("\\.");
+        if ((hmsAndMsec.length < 1) || (hmsAndMsec.length > 2)) {
+            logger.debug("Incorrect number of '.': " + value);
+            return false;
+        }
+        if (hmsAndMsec.length == 2) {
+            String milliseconds = hmsAndMsec[1];
+            if ((milliseconds.length() != 3) || (!Utils.isNonNegativeIntegerNumber(milliseconds))) {
+                logger.debug("Milliseconds are specified incorrectly: " + value);
+                return false;
+            }
+        }
+
+        String[] hms = hmsAndMsec[0].split(":");
+        if ((hms.length < 1) || (hms.length > 3)) {
+            logger.debug("Incorrect number of time parts: " + value);
+            return false;
+        }
+        for (String part : hms) {
+            if (!Utils.isNonNegativeIntegerNumber(part)) {
+                logger.debug("Some of the values is not a valid number: " + value);
+                return false;
+            }
+        }
+        String seconds = hms[hms.length - 1];
+        if ((hms.length > 1) && (seconds.length() != 2)) {
+            logger.debug("Incorrectly written seconds" + value);
+            return false;
+        }
+        if (Long.parseLong(seconds) >= 60) {
+            logger.debug("Exceeding the possible number of seconds: " + value);
+            return false;
+        }
+        if (hms.length >= 2) {
+            String minutes = hms[hms.length - 2];
+            if ((hms.length > 2) && (minutes.length() != 2)) {
+                logger.debug("Incorrectly written minutes: " + value);
+                return false;
+            }
+            if (Long.parseLong(minutes) >= 60) {
+                logger.debug("Exceeding the possible number of minutes: " + value);
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -78,7 +145,9 @@ public class Time {
      * @param other the value to add
      */
     public void add(Time other) {
-
+        if (other != null) {
+            setMilliseconds(getMilliseconds() + other.getMilliseconds());
+        }
     }
 
     /**
@@ -87,7 +156,10 @@ public class Time {
      * @param other the time to be subtracted from the current time
      */
     public void sub(Time other) {
-
+        if (other != null) {
+            Long newMillisecondsValue = Math.max(0L, getMilliseconds() - other.getMilliseconds());
+            setMilliseconds(newMillisecondsValue);
+        }
     }
 
     /**
@@ -96,7 +168,7 @@ public class Time {
      * @return the time in milliseconds
      */
     public Long getMilliseconds() {
-        return null;
+        return milliseconds;
     }
 
     /**
@@ -105,7 +177,21 @@ public class Time {
      * @return the time in the format (H...)H:MM:SS
      */
     public String getHmsFormat() {
-        return null;
+        Long fullSeconds = getMilliseconds() / 1000;
+        Long fullMinutes = fullSeconds / 60;
+        Long fullHours = fullMinutes / 60;
+
+        String seconds = String.valueOf(fullSeconds % 60);
+        if (seconds.length() == 1) {
+            seconds = "0" + seconds;
+        }
+        String minutes = String.valueOf(fullMinutes % 60);
+        if (minutes.length() == 1) {
+            minutes = "0" + minutes;
+        }
+        String hours = String.valueOf(fullHours);
+
+        return hours + ":" + minutes + ":" + seconds;
     }
 
     /**
@@ -114,7 +200,42 @@ public class Time {
      * @return the time in the format (M)M:SS
      */
     public String getMsFormat() {
-        return null;
+        Long fullSeconds = getMilliseconds() / 1000;
+        Long fullMinutes = fullSeconds / 60;
+
+        String seconds = String.valueOf(fullSeconds % 60);
+        if (seconds.length() == 1) {
+            seconds = "0" + seconds;
+        }
+        String minutes = String.valueOf(fullMinutes % 60);
+
+        return minutes + ":" + seconds;
+    }
+
+    private void setMilliseconds(Long value) {
+        milliseconds = value;
+    }
+
+    private Long timeAsStringToLong(String value) {
+        if (!isCorrectTime(value)) {
+            return 0L;
+        }
+
+        Long answer = 0L;
+        String[] hmsAndMsec = value.split("\\.");
+        if (hmsAndMsec.length == 2) {
+            answer += Long.parseLong(hmsAndMsec[1]);
+        }
+        String[] hms = hmsAndMsec[0].split(":");
+        answer += Long.parseLong(hms[hms.length - 1]) * 1000;
+        if (hms.length >= 2) {
+            answer += Long.parseLong(hms[hms.length - 2]) * 60 * 1000;
+        }
+        if (hms.length == 3) {
+            answer += Long.parseLong(hms[hms.length - 3]) * 60 * 60 * 1000;
+        }
+
+        return answer;
     }
 
     @Override

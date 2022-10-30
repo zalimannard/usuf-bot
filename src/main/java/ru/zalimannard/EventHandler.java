3c74a26d56d9c86e7bc9394ab7c4eb87f4e94193
Dmitry Kolesnikov <zalimannard@mail.ru>
2022-10-31 00:13:02 +0300

    feat: Add MessageSender to PlayerManagerManager

--- src/main/java/ru/zalimannard/EventHandler.java
+++ src/main/java/ru/zalimannard/EventHandler.java
@@ -1,6 +1,9 @@
 package ru.zalimannard;
 
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import ru.zalimannard.command.Command;
+import ru.zalimannard.command.CommandFactory;
 
 /**
  * The type Event handler. It reacts to events that occurred in the Discord.
@@ -17,6 +20,30 @@ public class EventHandler extends ListenerAdapter {
      */
     public EventHandler(String prefix) {
         this.prefix = prefix;
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        String[] messageTextList = event.getMessage().getContentRaw().split("\n");
+        for (String messageText : messageTextList) {
+            messageText = messageText.trim();
+            if (!messageText.startsWith(getPrefix())) {
+                continue;
+            }
+
+            // Update the channel for the bot's message output
+            if (PlayerManagerManager.getInstance().getMessageSender(event.getGuild().getId()) == null) {
+                PlayerManagerManager.getInstance().setMessageSender(event.getGuild().getId(),
+                        new MessageSender(prefix, event.getMessage().getChannel(), event.getGuild()));
+            } else {
+                PlayerManagerManager.getInstance().getMessageSender(event.getGuild().getId())
+                        .setCurrentMessageChannel(event.getMessage().getChannel());
+            }
+        }
     }
 
     /**
